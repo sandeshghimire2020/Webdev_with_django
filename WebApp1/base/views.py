86145a0fa5088386eaffa312799faed79f167cef
sandeshghimire2020 <sandesh.ghimire2020@gmail.com>
2022-04-15 17:34:20 -0500

    first

--- WebApp1/base/views.py
+++ WebApp1/base/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def home(request):
-    return render(request,'home.html')
 
-def room(request):
-    return render(request,'room.html')
+
+rooms = [
+    {
+        'id':'1',
+        'name': "Hello World!",
+        'description': "This is the description",
+    },
+    {
+        'id':'2',
+        'name': "city",
+        'description': "This is the description",
+    },
+    {
+        'id':'3',
+        'name': "Python",
+        'description': "This is the description",
+    },
+]
+
+def home(request):
+
+    return render(request,'base/home.html', {'rooms':rooms})
+
+def room(request,pk):
+    context = None
+    for i in rooms:
+        if i['id'] == pk:
+            context = i
+
+    return render(request,'base/room.html',{'cont':context})
