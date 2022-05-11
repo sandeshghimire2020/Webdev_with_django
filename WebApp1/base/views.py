1f6fa1d988a870676efed8ff275a3c4e1b7c34da
sandeshghimire2020 <sandesh.ghimire2020@gmail.com>
2022-05-11 11:04:54 -0500

    CURD

--- WebApp1/base/views.py
+++ WebApp1/base/views.py
@@ -30,12 +30,12 @@ def updateProject(request, pk):
     editProject = Project.objects.get(id=pk) #we are taking pk of the project where user clicked edit
     forms = ProjectForm(instance = editProject) #sending the instance that we got to the form
     if request.method =='POST':
-        forms = ProjectForm(request.POST)
+        forms = ProjectForm(request.POST, instance = editProject)
         if forms.is_valid():
             forms.save()
             return redirect('home')
     Context={'form':forms}
-    return render(request,'base/project_form.html',Context )
+    return render(request,'base/project_form.html',Context)
 
 def deleteProject (request, pk):
     deleteProject = Project.objects.get(id=pk)
