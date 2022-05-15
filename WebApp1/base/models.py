39b3321001c0c67449290556ab568cd30145d435
sandeshghimire2020 <sandesh.ghimire2020@gmail.com>
2022-05-15 15:56:10 -0500

    templates

--- WebApp1/base/models.py
+++ WebApp1/base/models.py
@@ -37,9 +37,7 @@ class Review(models.Model):
         return self.value
 
 
-class Tag(models.Model):
-
-   
+class Tag(models.Model):   
     name=models.CharField(max_length = 200)
     created = models.DateTimeField(auto_now_add = True)
     id = models.UUIDField(default=uuid.uuid4,unique=True,primary_key = True, editable=False)
