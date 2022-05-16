88db1dfca25f5348fe45d4383a59eed6a842b834
sandeshghimire2020 <sandesh.ghimire2020@gmail.com>
2022-05-16 16:56:50 -0500

    forms template

--- WebApp1/base/forms.py
+++ WebApp1/base/forms.py
@@ -1,9 +1,23 @@
+from select import select
 from django.forms import ModelForm
 from .models import Project
+from django import forms
 
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'userName', 'user_image','description',  'demo_link','source_link','tags']
+        widgets= {
+            'tags' : forms.CheckboxSelectMultiple(), 
+        }
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm,self).__init__(*args, **kwargs)
+        
+        
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class':'input'})
+        
+        ##self.fields['title'].widget.attrs.update({'class':'input','placeholder': 'Add title'})
+        
 
