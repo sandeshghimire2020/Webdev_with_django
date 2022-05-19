0a5c48feb031790c539e015ccdc4c2cc3aa84e75
sandeshghimire2020 <sandesh.ghimire2020@gmail.com>
2022-05-18 19:28:50 -0500

    profiles

--- WebApp1/users/urls.py
+++ WebApp1/users/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('',views.profiles, name="profiles")
-    
+    path('',views.profiles, name="profiles"),
 ]
--- WebApp1/users/views.py
+++ WebApp1/users/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Profile
 
 # Create your views here.
 def profiles(request):
-    return render(request,'users/profiles.html')
+    profileObj = Profile.objects.all();
+    Context={'Profile':profileObj}
+    return render(request,'users/profiles.html',Context)
+
