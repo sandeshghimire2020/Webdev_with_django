65c106c9f757dc37834ac024a9765500ce828544
sandeshghimire2020 <sandesh.ghimire2020@gmail.com>
2022-05-19 22:57:04 -0500

    user-profile

--- WebApp1/users/models.py
+++ WebApp1/users/models.py
@@ -8,6 +8,7 @@ class Profile(models.Model):
     user= models.OneToOneField(User,on_delete=models.CASCADE, null=True, blank=True) ##creating one to one relationship with user
     name = models.CharField(max_length=200,blank=True,null=True)
     username = models.CharField(max_length=200, null = True, blank= True)
+    location = models.CharField(max_length=200, null = True, blank= True)
     email=models.EmailField(max_length=500,blank=True,null=True)
     short_intro = models.CharField(max_length=200,blank=True,null=True)
     bio=models.TextField(blank=True,null=True)
@@ -22,5 +23,17 @@ class Profile(models.Model):
     
     def __str__(self):
         return str(self.user.username)
+
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE,null=True,blank=True)
+    name=models.CharField(max_length=200,blank=True,null=True)
+    description=models.TextField(blank=True,null=True)
+    created = models.DateTimeField(auto_now_add = True)
+    id = models.UUIDField(default=uuid.uuid4,unique=True,primary_key = True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
+
     
     
--- WebApp1/users/views.py
+++ WebApp1/users/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-from .models import Profile
+from .models import Profile, Skill
 
 # Create your views here.
 def profiles(request):
     profileObj = Profile.objects.all();
     Context={'Profile':profileObj}
     return render(request,'users/profiles.html',Context)
+
+def userProfile(request,pk):
+    profileObject = Profile.objects.get(id=pk)
+    
+    topSkills = profileObject.skill_set.exclude(description__exact="")
+    otherSkills=profileObject.skill_set.filter(description="")
+    Context={'userProfile':profileObject,'topSkills':topSkills,'otherSkills':otherSkills}
+    return render(request,'users/Dev.html',Context)
+
 
