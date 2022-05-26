7fdced59bbd35d0b3bdd41ff680fc0550f45964c
sandeshghimire2020 <sandesh.ghimire2020@gmail.com>
2022-05-26 12:49:20 -0500

    User Login

--- WebApp1/base/views.py
+++ WebApp1/base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from .models import Project
 from .forms import ProjectForm
 # Create your views here.
@@ -17,6 +18,7 @@ def project(request,pk):
     ProjectObj=Project.objects.get(id=pk)
     return render(request,'base/Project.html',{'cont':ProjectObj})
 
+@login_required(login_url="login")
 def createProject(request):
     form = ProjectForm()
     if request.method =='POST':
@@ -27,6 +29,7 @@ def createProject(request):
     Context={'form':form}
     return render(request,"base/project_form.html",Context )
 
+@login_required(login_url="login")
 def updateProject(request, pk):
     editProject = Project.objects.get(id=pk) #we are taking pk of the project where user clicked edit
     forms = ProjectForm(instance = editProject) #sending the instance that we got to the form
@@ -38,6 +41,7 @@ def updateProject(request, pk):
     Context={'form':forms}
     return render(request,"base/project_form.html",Context)
 
+@login_required(login_url="login")
 def deleteProject (request, pk):
     deleteProject = Project.objects.get(id=pk)
     if request.method == 'POST':
--- WebApp1/users/views.py
+++ WebApp1/users/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Profile, Skill
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 def profiles(request):
@@ -15,4 +18,36 @@ def userProfile(request,pk):
     Context={'userProfile':profileObject,'topSkills':topSkills,'otherSkills':otherSkills}
     return render(request,'users/Dev.html',Context)
 
+
+
+def loginUser(request):
+    if request.user.is_authenticated:
+        return redirect('profiles')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username) #quering the username out
+        except:
+            messages.error(request,"Username doesn't exist") #if username doesn't exist print this
+
+        #this will authenticate if password matches username
+        #returns either user instance or non
+        user=authenticate(request, username = username, password=password) 
+
+        if user is not None:
+            # this function will create a session if the user exists
+            #after that it will add session key to our browser cookies
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request,"Username or password is incorrect")
+
+    return render(request, 'users/login_register.html')
+
+def logoutUser(request):
+    logout(request) #delete the session
+    messages.error(request,"User was logged out!")
+    return redirect('login')
 
