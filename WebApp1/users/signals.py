03a72f2fb441dfde2265b16dc3ed759bfddf6a8c
sandeshghimire2020 <sandesh.ghimire2020@gmail.com>
2022-06-05 11:25:33 -0500

    sending emails

--- WebApp1/users/signals.py
+++ WebApp1/users/signals.py
@@ -3,6 +3,8 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
+from django.core.mail import send_mail
+from django.conf import settings
 #@receiver(post_save, sender=Profile)
 def createProfile(sender, instance, created, **kwargs):
     print("Profile signal connected.")
@@ -14,6 +16,15 @@ def createProfile(sender, instance, created, **kwargs):
             email=user.email,
             name=user.first_name,
             
+        )
+        subject='Welcome to devSearch'
+        message='Thank you for joining!'
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
         )
 def updateUser(sender, instance, created,**kwargs):
     profile = instance
